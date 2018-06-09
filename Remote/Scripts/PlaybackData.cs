747ff54a4e7144f48a5f9c446413c4fa9932dfda
Jonathan Newberry <jonathann@unity3d.com>
2018-06-08 22:48:44 -0700

    changes to recording to allow multiple captures in a single session

--- Remote/Scripts/PlaybackData.cs
+++ Remote/Scripts/PlaybackData.cs
@@ -15,12 +15,18 @@ namespace Unity.Labs.FacialRemote
 
         public PlaybackBuffer[] playbackBuffers { get { return m_PlaybackBuffers; } }
 
-        PlaybackBuffer m_ActiveBuffer;
-        public List<byte[]> activeByteRecord { get; private set; }
+        public List<byte[]> activeByteRecord
+        {
+            get
+            {
+                return m_ActiveRecords.Count > 0 ? m_ActiveRecords.Last() : null;
+            }
+        }
 
-        [SerializeField]
-        [HideInInspector]
-        byte[] m_LastRecord;
+//        List<byte[]> m_ActiveByteRecord;
+
+        Dictionary<PlaybackBuffer, List<byte[]>> m_RecordTakes = new Dictionary<PlaybackBuffer, List<byte[]>>();
+        List<List<byte[]>> m_ActiveRecords = new List<List<byte[]>>();
 
         void OnEnable()
         {
@@ -39,45 +45,48 @@ namespace Unity.Labs.FacialRemote
 #if UNITY_EDITOR
         void EditorStateChange(PlayModeStateChange state)
         {
-            byte errorCheck = 42;
             if (state == PlayModeStateChange.ExitingPlayMode)
             {
-                if (m_ActiveBuffer != null && activeByteRecord != null)
+                if (m_RecordTakes.Count == 0)
+                    return;
+
+                var newBuffers = new List<PlaybackBuffer>();
+                foreach (var take in m_RecordTakes)
                 {
-                    m_LastRecord = new byte[activeByteRecord.Count * m_ActiveBuffer.BufferSize];
-                    var buffer = new byte[m_ActiveBuffer.BufferSize];
-                    for (var i = 0; i < activeByteRecord.Count; i++)
+                    var playbackBuffer = take.Key;
+                    var byteRecord = take.Value;
+                    var record = new byte[byteRecord.Count * playbackBuffer.BufferSize];
+                    for (var i = 0; i < byteRecord.Count; i++)
                     {
-                        buffer = activeByteRecord[i];
-                        Buffer.BlockCopy(buffer, 0, m_LastRecord, i * m_ActiveBuffer.BufferSize, m_ActiveBuffer.BufferSize);
-                        if (buffer[0] != errorCheck)
+                        var buffer = byteRecord[i];
+                        Buffer.BlockCopy(buffer, 0, record, i * playbackBuffer.BufferSize, playbackBuffer.BufferSize);
+                        if (buffer[0] != playbackBuffer.ErrorCheck)
                             Debug.LogError(string.Format("Error in buffer {0}", i));
                     }
-                      //  activeByteQueue.SelectMany(s => s).ToArray();
-                    m_ActiveBuffer.recordStream = m_LastRecord.ToArray();
-                    EditorUtility.SetDirty(this);
+                    playbackBuffer.recordStream = record.ToArray();
 
-                    if (m_ActiveBuffer.recordStream.Length < 1 && m_PlaybackBuffers.Contains(m_ActiveBuffer))
+                    if (playbackBuffer.recordStream.Length > 0)
                     {
-                        m_PlaybackBuffers = m_PlaybackBuffers.Where(s => s != m_ActiveBuffer).ToArray();
+                        newBuffers.Add(playbackBuffer);
                     }
-
-                    m_ActiveBuffer = null;
-                    activeByteRecord.Clear();
                 }
+
+                m_PlaybackBuffers = newBuffers.Concat(m_PlaybackBuffers).Where(w => w.recordStream.Length > 0).ToArray();
+                m_RecordTakes.Clear();
+                m_ActiveRecords.Clear();
+                EditorUtility.SetDirty(this);
             }
         }
 #endif
 
-        public void CreatePlaybackBuffer(IStreamSettings streamSettings)
+        public void CreatePlaybackBuffer(IStreamSettings streamSettings, int take)
         {
-            var buffers = m_PlaybackBuffers.ToList();
-            var buffer = new PlaybackBuffer(streamSettings);
-            buffer.name = DateTime.Now.ToString("yyyyMMddHHmmssffff");
-            buffers.Add(buffer);
-            m_PlaybackBuffers = buffers.ToArray();
-            m_ActiveBuffer = m_PlaybackBuffers[m_PlaybackBuffers.Length-1];
-            activeByteRecord = new List<byte[]>();
+            var playbackBuffer = new PlaybackBuffer(streamSettings);
+            playbackBuffer.name = String.Format("{0:yyyy_MM_dd_HH_mm}-Take{1:00}", DateTime.Now, take);
+            Debug.Log(string.Format("Starting take: {0}", playbackBuffer.name));
+            var byteRecord = new List<byte[]>();
+            m_RecordTakes.Add(playbackBuffer, byteRecord);
+            m_ActiveRecords.Add(byteRecord);
         }
     }
 }
