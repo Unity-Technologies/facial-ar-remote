cc96bc69bc3ca0d8821cf4a7b0f4632782bb1362
Matt Schoen <schoen@unity3d.com>
2018-06-14 11:11:05 -0700

    Bring back lost changes to Fix hang/crash on bad connection

--- Remote/Scripts/ClientGUI.cs
+++ Remote/Scripts/ClientGUI.cs
@@ -1,5 +1,7 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 using UnityEngine;
 
 namespace Unity.Labs.FacialRemote
@@ -10,13 +12,15 @@ namespace Unity.Labs.FacialRemote
         int m_Port = 9000;
 
         [SerializeField]
-        string m_IP = "192.168.1.1";
+        string m_IP = "10.0.1.3";
 
         [SerializeField]
         Client m_Client;
 
         [SerializeField]
         GUIStyle m_Style = GUIStyle.none;
+
+        Socket m_Socket;
 
         void OnGUI()
         {
@@ -29,19 +33,48 @@ namespace Unity.Labs.FacialRemote
             GUILayout.Label("IP address:", m_Style);
             m_IP = GUILayout.TextField(m_IP, m_Style);
 
-            IPAddress address;
-            GUI.enabled = IPAddress.TryParse(m_IP, out address);
-
-            if (GUILayout.Button("Connect", m_Style))
+            if (m_Socket == null)
             {
-                var endPoint = new IPEndPoint(address, m_Port);
-                var socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-                socket.Connect(endPoint);
-                m_Client.Setup(socket);
+                IPAddress address;
+                GUI.enabled = IPAddress.TryParse(m_IP, out address);
+                if (GUILayout.Button("Connect", m_Style))
+                {
+                    new Thread(() =>
+                    {
+                        try
+                        {
+                            var endPoint = new IPEndPoint(address, m_Port);
+                            m_Socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                            m_Socket.Connect(endPoint);
+                        }
+                        catch (Exception e)
+                        {
+                            Debug.Log("Exception trying to connect: " + e.Message);
+                            m_Socket = null;
+                        }
+                    }).Start();
+                }
+
+                GUI.enabled = true;
+            }
+            else
+            {
+                if (GUILayout.Button("Cancel", m_Style))
+                {
+                    m_Socket.Disconnect(false);
+                    m_Socket = null;
+                }
+            }
+        }
+
+        void Update()
+        {
+            if (m_Socket != null && m_Socket.Connected)
+            {
+                m_Client.Setup(m_Socket);
+                m_Socket = null;
                 enabled = false;
             }
-
-            GUI.enabled = true;
         }
     }
 }
