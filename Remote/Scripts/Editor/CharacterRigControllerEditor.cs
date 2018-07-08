93b330a6272e56b76050cc39d2cbbd58cb1c5a72
Jonathan Newberry <jonathann@unity3d.com>
2018-07-07 22:21:33 -0700

    minor improvements to eye look and character rig controller editor.

--- Remote/Scripts/Editor/CharacterRigControllerEditor.cs
+++ Remote/Scripts/Editor/CharacterRigControllerEditor.cs
@@ -14,6 +14,7 @@ namespace Unity.Labs.FacialRemote
         SerializedProperty m_RightEyeNegZ;
         SerializedProperty m_LeftEyeNegZ;
         SerializedProperty m_EyeAngleRange;
+        SerializedProperty m_EyeLookDistance;
         SerializedProperty m_DriveHead;
         SerializedProperty m_HeadBone;
         SerializedProperty m_HeadFollowAmount;
@@ -25,7 +26,7 @@ namespace Unity.Labs.FacialRemote
         bool m_HeadFoldout;
         bool m_NeckFoldout;
 
-        void Awake()
+        void OnEnable()
         {
             m_HeadBone = serializedObject.FindProperty("m_HeadBone");
             m_NeckBone = serializedObject.FindProperty("m_NeckBone");
@@ -38,6 +39,7 @@ namespace Unity.Labs.FacialRemote
             m_LeftEyeNegZ = serializedObject.FindProperty("m_LeftEyeNegZ");
             m_EyeAngleRange = serializedObject.FindProperty("m_EyeAngleRange");
             m_EyeSmoothing = serializedObject.FindProperty("m_EyeSmoothing");
+            m_EyeLookDistance = serializedObject.FindProperty("m_EyeLookDistance");
 
             m_DriveHead = serializedObject.FindProperty("m_DriveHead");
             m_HeadFollowAmount = serializedObject.FindProperty("m_HeadFollowAmount");
@@ -49,36 +51,45 @@ namespace Unity.Labs.FacialRemote
 
         public override void OnInspectorGUI()
         {
-            EditorGUILayout.LabelField("General Settings", EditorStyles.boldLabel);
-            EditorGUILayout.PropertyField(m_HeadBone);
-            EditorGUILayout.PropertyField(m_NeckBone);
-            EditorGUILayout.PropertyField(m_LeftEye);
-            EditorGUILayout.PropertyField(m_RightEye);
-            EditorGUILayout.PropertyField(m_TrackingLossSmoothing);
-
-            m_EyeFoldout = EditorGUILayout.Foldout(m_EyeFoldout, "Eye Settings");
-            if (m_EyeFoldout)
+            using (var check =  new EditorGUI.ChangeCheckScope())
             {
-                EditorGUILayout.PropertyField(m_DriveEyes);
-                EditorGUILayout.PropertyField(m_RightEyeNegZ);
-                EditorGUILayout.PropertyField(m_LeftEyeNegZ);
-                EditorGUILayout.PropertyField(m_EyeAngleRange);
-                EditorGUILayout.PropertyField(m_EyeSmoothing);
-            }
+                EditorGUILayout.LabelField("General Settings", EditorStyles.boldLabel);
+                EditorGUILayout.PropertyField(m_HeadBone);
+                EditorGUILayout.PropertyField(m_NeckBone);
+                EditorGUILayout.PropertyField(m_LeftEye);
+                EditorGUILayout.PropertyField(m_RightEye);
+                EditorGUILayout.PropertyField(m_TrackingLossSmoothing);
 
-            m_HeadFoldout = EditorGUILayout.Foldout(m_HeadFoldout, "Head Settings");
-            if (m_HeadFoldout)
-            {
-                EditorGUILayout.PropertyField(m_DriveHead);
-                EditorGUILayout.PropertyField(m_HeadFollowAmount);
-                EditorGUILayout.PropertyField(m_HeadSmoothing);
-            }
+                m_EyeFoldout = EditorGUILayout.Foldout(m_EyeFoldout, "Eye Settings");
+                if (m_EyeFoldout)
+                {
+                    EditorGUILayout.PropertyField(m_DriveEyes);
+                    EditorGUILayout.PropertyField(m_EyeLookDistance);
+                    EditorGUILayout.PropertyField(m_RightEyeNegZ);
+                    EditorGUILayout.PropertyField(m_LeftEyeNegZ);
+                    EditorGUILayout.PropertyField(m_EyeAngleRange);
+                    EditorGUILayout.PropertyField(m_EyeSmoothing);
+                }
 
-            m_NeckFoldout = EditorGUILayout.Foldout(m_NeckFoldout, "Neck Settings");
-            if (m_NeckFoldout)
-            {
-                EditorGUILayout.PropertyField(m_DriveNeck);
-                EditorGUILayout.PropertyField(m_NeckFollowAmount);
+                m_HeadFoldout = EditorGUILayout.Foldout(m_HeadFoldout, "Head Settings");
+                if (m_HeadFoldout)
+                {
+                    EditorGUILayout.PropertyField(m_DriveHead);
+                    EditorGUILayout.PropertyField(m_HeadFollowAmount);
+                    EditorGUILayout.PropertyField(m_HeadSmoothing);
+                }
+
+                m_NeckFoldout = EditorGUILayout.Foldout(m_NeckFoldout, "Neck Settings");
+                if (m_NeckFoldout)
+                {
+                    EditorGUILayout.PropertyField(m_DriveNeck);
+                    EditorGUILayout.PropertyField(m_NeckFollowAmount);
+                }
+
+                if (check.changed)
+                {
+                    serializedObject.ApplyModifiedProperties();
+                }
             }
         }
     }
