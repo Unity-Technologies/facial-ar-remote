5aecf0e834ca6dc8f1553eaf2be6268bc7de8971
Brad Weiers <bradley@unity3d.com>
2019-05-17 04:18:55 -0700

    Create separate scriptable object for blend shape mappings
    Allows separate assets for different rigs

---
+++ Editor/BlendShapeMappingsEditor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Unity.Labs.FacialRemote
+{
+    [CustomEditor(typeof(BlendShapeMappings))]
+    public class BlendShapeMappingsEditor : Editor
+    {
+        SerializedProperty m_LocationIdentifiers;
+        SerializedProperty m_BlendShapeNames;
+
+        void OnEnable()
+        {
+            m_LocationIdentifiers = serializedObject.FindProperty("m_LocationIdentifiers");
+            m_BlendShapeNames = serializedObject.FindProperty("m_BlendShapeNames");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            using (var check = new EditorGUI.ChangeCheckScope())
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("Location Identifier");
+                EditorGUILayout.LabelField("BlendShape Name");
+                EditorGUILayout.EndHorizontal();
+                for (var i = 0; i < m_LocationIdentifiers.arraySize; i++)
+                {
+                    EditorGUILayout.BeginHorizontal();
+                    EditorGUILayout.PropertyField(m_LocationIdentifiers.GetArrayElementAtIndex(i), new GUIContent());
+                    EditorGUILayout.PropertyField(m_BlendShapeNames.GetArrayElementAtIndex(i), new GUIContent());
+                    EditorGUILayout.EndHorizontal();
+                }
+
+                if (check.changed)
+                    serializedObject.ApplyModifiedProperties();
+            }
+        }
+    }
+}
---
+++ Remote/Scripts/BlendShapeMappings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unity.Labs.FacialRemote
+{
+    [Serializable]
+    [CreateAssetMenu(fileName = "BlendShape Mappings", menuName = "AR Face Capture/BlendShape Mappings")]
+    public class BlendShapeMappings : ScriptableObject
+    {
+        [SerializeField]
+        [Tooltip("String names of the blend shapes in the stream with their index in the array being their relative location.")]
+        string[] m_LocationIdentifiers;
+
+        [SerializeField]
+        [Tooltip("Rename mapping values to apply blend shape locations to a blend shape controller.")]
+        string[] m_BlendShapeNames;
+
+        public string[] blendShapeNames { get { return m_BlendShapeNames; }}
+
+        public string[] locationIdentifiers
+        {
+            get
+            {
+                return m_LocationIdentifiers;
+            }
+        }
+
+#if UNITY_EDITOR
+        void OnValidate()
+        {
+            UnityEditor.EditorUtility.SetDirty(this);
+        }
+#endif
+    }
+}
