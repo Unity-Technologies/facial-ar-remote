dd65b01596d22ff252a327233b3c6bbd524d2821
Sergi Valls <sergi@unity3d.com>
2019-07-19 10:33:40 +0800

    Sequential layout for FaceData

--- Runtime/Scripts/new/Data/FaceData.cs
+++ Runtime/Scripts/new/Data/FaceData.cs
@@ -3,13 +3,13 @@ using Unity.Labs.FacialRemote;
 
 namespace PerformanceRecorder
 {
-    [StructLayout(LayoutKind.Explicit)]
+    [StructLayout(LayoutKind.Sequential)]
     public struct FaceData
     {
         public static readonly PacketDescriptor Descriptor = new PacketDescriptor() { type = PacketType.Face, version = 0 };
-        
-        [FieldOffset(0)] public float timeStamp;
-        [FieldOffset(4)] public int id;
-        [FieldOffset(8)] public BlendShapeValues blendShapeValues;
+
+        public float timeStamp;
+        public int id;
+        public BlendShapeValues blendShapeValues;
     }
 }
