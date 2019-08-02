a4cc575873c7c9d4a9960b24760efee8eef1a162
Sergi Valls <sergi@unity3d.com>
2019-08-02 15:49:26 +0800

    Trying FaceDataRecorder

--- Runtime/Scripts/new/XRStream.cs
+++ Runtime/Scripts/new/XRStream.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
 using UnityEngine;
 using Unity.Labs.FacialRemote;
 
@@ -10,6 +12,8 @@ namespace PerformanceRecorder
         RemoteStream m_RemoteStream = new RemoteStream();
         [SerializeField]
         List<BlendShapesController> m_BlendShapesControllers = new List<BlendShapesController>();
+        FaceDataRecorder m_FaceDataRecorder = new FaceDataRecorder();
+        int m_TakeCount;
         
         public BlendShapesController[] blendShapesControllers
         {
@@ -38,10 +42,43 @@ namespace PerformanceRecorder
 
         void FaceDataChanged(FaceData faceData)
         {
+            if (m_FaceDataRecorder.isRecording)
+                m_FaceDataRecorder.Record(faceData);
+
             foreach (var blendShapesController in m_BlendShapesControllers)
             {
                 blendShapesController.blendShapeInput = faceData.blendShapeValues;
             }
+        }
+
+        [ContextMenu("Start Recording")]
+        public void StartRecoding()
+        {
+            m_TakeCount++;
+            m_FaceDataRecorder.StartRecording();
+        }
+
+        [ContextMenu("Stop Recording")]
+        public void StopRecording()
+        {
+            m_FaceDataRecorder.StopRecording();
+        }
+
+        [ContextMenu("Save Recording")]
+        public void SaveRecording()
+        {
+            var path = "Assets/" + GenerateFileName() +".arstream";
+
+            using (var fileStream = File.Create(path))
+            {
+                var buffer = m_FaceDataRecorder.GetBuffer();
+                fileStream.Write(buffer, 0, buffer.Length);
+            }
+        }
+
+        string GenerateFileName()
+        {
+            return string.Format("{0:yyyy_MM_dd_HH_mm}-Take{1:00}", DateTime.Now, m_TakeCount);
         }
     }
 }
