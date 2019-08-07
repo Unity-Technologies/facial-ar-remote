d9b6beddcd51e296dd259e37a5401e9a5a745bc1
Sergi Valls <sergi@unity3d.com>
2019-08-07 17:25:27 +0800

    Decouple editor window from actor state.

--- Editor/new/ClientWindow.cs
+++ Editor/new/ClientWindow.cs
@@ -7,19 +7,205 @@ using Unity.Labs.FacialRemote;
 
 namespace PerformanceRecorder
 {
+    public enum PreviewState
+    {
+        None,
+        Playback,
+        LiveStream
+    }
+
+    public class RemoteActor
+    {
+        static readonly string k_DefaultDirectory = "Assets/";
+        RemoteStream m_Stream = new RemoteStream();
+        FaceDataRecorder m_Recoder = new FaceDataRecorder();
+        TakePlayer m_Player = new TakePlayer();
+        BlendShapesController m_Controller;
+        PreviewState m_PrevState = PreviewState.None;
+        AnimationClip m_Clip;
+        string m_Directory = k_DefaultDirectory;
+        bool m_ChangingState = false;
+
+        public TakePlayer player
+        {
+            get { return m_Player; }
+        }
+
+        public PreviewState state
+        {
+            get { return m_PrevState; }
+        }
+
+        public BlendShapesController controller
+        {
+            get { return m_Controller; }
+            set { m_Controller = value; }
+        }
+
+        public AnimationClip clip
+        {
+            get { return m_Clip; }
+            set { m_Clip = value; }
+        }
+
+        public string directory
+        {
+            get { return m_Directory; }
+            set { m_Directory = value; }
+        }
+
+        public RemoteActor()
+        {
+            m_Stream.reader.faceDataChanged += FaceDataChanged;
+        }
+
+        ~RemoteActor()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            Disconnect();
+            m_Stream.reader.faceDataChanged -= FaceDataChanged;
+        }
+
+        void SetPreviewState(PreviewState newState)
+        {
+            if (m_ChangingState)
+                return;
+            
+            if (m_PrevState == newState)
+                return;
+            
+            m_ChangingState = true;
+
+            if (m_PrevState == PreviewState.LiveStream)
+            {
+                StopRecording();
+            }
+            else if (m_PrevState == PreviewState.Playback)
+            {
+                if (m_Player.isPlaying)
+                    m_Player.Stop();
+            }
+
+            if (newState == PreviewState.LiveStream)
+            {
+
+            }
+            else if (newState == PreviewState.Playback)
+            {
+                var animator = m_Controller.GetComponent<Animator>();
+                m_Player.Play(animator, m_Clip);
+            }
+
+            m_PrevState = newState;
+            m_ChangingState = false;
+        }
+
+        public void Connect()
+        {
+            m_Stream.isServer = true;
+            m_Stream.Connect();
+            SetPreviewState(PreviewState.LiveStream);
+        }
+
+        public void Disconnect()
+        {
+            m_Stream.Disconnect();
+            SetPreviewState(PreviewState.None);
+        }
+
+        public void Update()
+        {
+            m_Stream.reader.Receive();
+            
+            if (m_PrevState == PreviewState.Playback)
+                m_Player.Update();    
+        }
+
+        public void StartRecording()
+        {
+            if (m_PrevState != PreviewState.LiveStream)
+                return;
+
+            m_Recoder.StartRecording();
+        }
+
+        public bool IsRecording()
+        {
+            return m_Recoder.isRecording;
+        }
+
+        public void StopRecording()
+        {
+            if (!m_Recoder.isRecording)
+                return;
+            
+            m_Recoder.StopRecording();
+        }
+
+        public byte[] GetRecording()
+        {
+            return m_Recoder.GetBuffer();
+        }
+
+        public void StartLiveStream()
+        {
+            SetPreviewState(PreviewState.LiveStream);
+        }
+
+        public void StopLiveStream()
+        {
+            if (m_PrevState != PreviewState.LiveStream)
+                return;
+
+            SetPreviewState(PreviewState.None);
+        }
+
+        public void StartPlayback()
+        {
+            SetPreviewState(PreviewState.Playback);
+        }
+
+        public void StopPlayback()
+        {
+            if (m_PrevState != PreviewState.Playback)
+                return;
+            
+            SetPreviewState(PreviewState.None);
+        }
+
+        public void PausePlayback()
+        {
+            m_Player.Pause();
+        }
+
+        void FaceDataChanged(FaceData data)
+        {
+            if (m_Controller == null)
+                return;
+
+            if (state != PreviewState.LiveStream)
+                return;
+
+            m_Controller.blendShapeInput = data.blendShapeValues;
+            m_Controller.UpdateBlendShapes();
+
+            if (m_Recoder.isRecording)
+                m_Recoder.Record(data);
+        }
+    }
+
     public class ClientWindow : EditorWindow
     {
         static readonly GUILayoutOption kButtonSmall = GUILayout.Width(20f);
         static readonly GUILayoutOption kButtonMid = GUILayout.Width(36f);
         static readonly GUILayoutOption kButtonWide = GUILayout.Width(60f);
         static readonly string kAssets = "Assets";
-        RemoteStream m_Server = new RemoteStream();
         RemoteStream m_Client = new RemoteStream();
-        FaceDataRecorder m_Recoder = new FaceDataRecorder();
-        string m_RecorderDirectory = kAssets;
-        TakePlayer m_Player = new TakePlayer();
-        AnimationClip m_Clip;
-        BlendShapesController m_Controller;
+        RemoteActor m_Actor = new RemoteActor();
 
         [MenuItem("Window/Test Client")]
         public static void ShowWindow()
@@ -31,24 +217,21 @@ namespace PerformanceRecorder
 
         void OnEnable()
         {
-            m_Server.reader.faceDataChanged += FaceDataChanged;
-
             EditorApplication.update += Update;
         }
 
         void OnDisable()
         {
-            m_Server.reader.faceDataChanged -= FaceDataChanged;
-
-            m_Server.Disconnect();
+            m_Actor.Dispose();
+            AnimationMode.StopAnimationMode();
 
             EditorApplication.update -= Update;
         }
 
         void FindController()
         {
-            if (m_Controller == null)
-                m_Controller = GameObject.FindObjectOfType<BlendShapesController>();
+            if (m_Actor.controller == null)
+                m_Actor.controller = GameObject.FindObjectOfType<BlendShapesController>();
         }
 
         void OnGUI()
@@ -64,49 +247,33 @@ namespace PerformanceRecorder
 
                 EditorGUILayout.LabelField("Recorder", EditorStyles.boldLabel);
                 DoDirectoryGUI();
-                DoRecorderGUI();
 
-                EditorGUILayout.LabelField("Preview", EditorStyles.boldLabel);
-                DoPlayerGUI();
+                using (new EditorGUI.DisabledGroupScope(m_Actor.state != PreviewState.LiveStream))
+                {    
+                    DoRecorderGUI();
+                }
+
+                using (new EditorGUI.DisabledGroupScope(m_Actor.state == PreviewState.LiveStream))
+                {
+                    EditorGUILayout.LabelField("Preview", EditorStyles.boldLabel);
+                    DoPlayerGUI();
+                }
             }
+
+            /*
             using (new GUILayout.VerticalScope("box"))
             {
                 EditorGUILayout.LabelField("Client", EditorStyles.boldLabel);
                 ClientGUI();
             }
+            */
 
             EditorGUIUtility.labelWidth = 0f;
         }
 
         void Update()
         {
-            m_Server.reader.Receive();
-
-            if (EditorApplication.isPlayingOrWillChangePlaymode)
-                Stop();
-
-            if (m_Player.isPlaying)
-                m_Player.Update();
-        }
-
-        void FaceDataChanged(FaceData data)
-        {
-            if (m_Controller != null)
-            {
-                Stop();
-
-                if (!AnimationMode.InAnimationMode())
-                {
-                    AnimationMode.StartAnimationMode();
-                    PrepareAnimationModeCurveBindings(m_Controller.gameObject);
-                }
-
-                m_Controller.blendShapeInput = data.blendShapeValues;
-                m_Controller.UpdateBlendShapes();
-            }
-
-            if (m_Recoder.isRecording)
-                m_Recoder.Record(data);
+            m_Actor.Update();
         }
 
         void DeviceGUI()
@@ -115,22 +282,23 @@ namespace PerformanceRecorder
             {
                 GUILayout.FlexibleSpace();
 
-                using (new EditorGUI.DisabledGroupScope(m_Server.isListening))
+                using (new EditorGUI.DisabledGroupScope(m_Actor.state != PreviewState.None))
                 {
                     if (GUILayout.Button("Connect", EditorStyles.miniButton, kButtonWide))
                     {
-                        m_Server.isServer = true;
-                        m_Server.Connect();
+                        m_Actor.Connect();
+
+                        AnimationMode.StartAnimationMode();
+                        PrepareAnimationModeCurveBindings(m_Actor.controller.gameObject);
                     }
                 }
-                using (new EditorGUI.DisabledGroupScope(!m_Server.isListening))
+                using (new EditorGUI.DisabledGroupScope(m_Actor.state != PreviewState.LiveStream))
                 {
                     if (GUILayout.Button("Disconnect", EditorStyles.miniButton, kButtonWide))
                     {
-                        m_Server.Disconnect();
+                        m_Actor.Disconnect();
 
-                        if (AnimationMode.InAnimationMode())
-                            AnimationMode.StopAnimationMode();
+                        AnimationMode.StopAnimationMode();
                     }
                 }
             }
@@ -141,17 +309,17 @@ namespace PerformanceRecorder
         {
             using (new EditorGUILayout.HorizontalScope())
             {
-                EditorGUILayout.TextField("Directory", m_RecorderDirectory);
+                EditorGUILayout.TextField("Directory", m_Actor.directory);
 
                 if (GUILayout.Button("o", EditorStyles.miniButton, kButtonSmall))
                 {
-                    var path = EditorUtility.OpenFolderPanel("Record Directory", m_RecorderDirectory, "");
+                    var path = EditorUtility.OpenFolderPanel("Record Directory", m_Actor.directory, "");
 
-                    if (path != m_RecorderDirectory)
+                    if (path != m_Actor.directory)
                     {
                         var index = path.IndexOf(kAssets);
                         
-                        m_RecorderDirectory = path.Substring(index) + "/";
+                        m_Actor.directory = path.Substring(index) + "/";
                     }
                 }
             }
@@ -159,38 +327,37 @@ namespace PerformanceRecorder
 
         void DoRecorderGUI()
         {
-            using (new EditorGUI.DisabledGroupScope(!m_Player.isStopped))
             using (new EditorGUILayout.HorizontalScope())
             {
                 GUILayout.FlexibleSpace();
 
-                using (new EditorGUI.DisabledGroupScope(m_Recoder.isRecording))
+                using (new EditorGUI.DisabledGroupScope(m_Actor.IsRecording()))
                 {
                     if (GUILayout.Button("Record", EditorStyles.miniButton, kButtonWide))
                     {
-                        m_Recoder.StartRecording();
+                        m_Actor.StartRecording();
                     }
                 }
-                using (new EditorGUI.DisabledGroupScope(!m_Recoder.isRecording))
+                using (new EditorGUI.DisabledGroupScope(!m_Actor.IsRecording()))
                 {
                     if (GUILayout.Button("Stop", EditorStyles.miniButton, kButtonMid))
                     {
-                        var uniqueAssetPath = AssetDatabase.GenerateUniqueAssetPath(m_RecorderDirectory + GenerateFileName());
+                        m_Actor.StopRecording();
+
+                        var uniqueAssetPath = AssetDatabase.GenerateUniqueAssetPath(m_Actor.directory + GenerateFileName());
                         var path = uniqueAssetPath + ".arstream";
 
-                        m_Recoder.StopRecording();
-                        
                         using (var fileStream = File.Create(path))
                         {
-                            var buffer = m_Recoder.GetBuffer();
+                            var buffer = m_Actor.GetRecording();
                             fileStream.Write(buffer, 0, buffer.Length);
                         }
 
                         AssetDatabase.Refresh();
 
-                        m_Clip = AssetDatabase.LoadAssetAtPath<AnimationClip>(path);
+                        m_Actor.clip = AssetDatabase.LoadAssetAtPath<AnimationClip>(path);
 
-                        EditorGUIUtility.PingObject(m_Clip);
+                        EditorGUIUtility.PingObject(m_Actor.clip);
                     }
                 }
             }
@@ -198,45 +365,53 @@ namespace PerformanceRecorder
 
         void DoPlayerGUI()
         {
-            using (new EditorGUI.DisabledGroupScope(m_Recoder.isRecording))
+            using (var change = new EditorGUI.ChangeCheckScope())
             {
-                using (var change = new EditorGUI.ChangeCheckScope())
+                m_Actor.clip = EditorGUILayout.ObjectField("Clip", m_Actor.clip, typeof(AnimationClip), true) as AnimationClip;
+
+                if (change.changed)
                 {
-                    m_Clip = EditorGUILayout.ObjectField("Clip", m_Clip, typeof(AnimationClip), true) as AnimationClip;
+                    var wasPlaying = m_Actor.state == PreviewState.Playback;
 
-                    if (change.changed)
+                    m_Actor.StopPlayback();
+
+                    if (wasPlaying)
+                        m_Actor.StartPlayback();
+                }
+            }
+
+            using (new EditorGUILayout.HorizontalScope())
+            using (new EditorGUI.DisabledGroupScope(m_Actor.clip == null || m_Actor.controller == null))
+            {
+                GUILayout.FlexibleSpace();
+
+                if (m_Actor.player.isPlaying)
+                {
+                    if (GUILayout.Button("Pause", EditorStyles.miniButton, kButtonMid))
                     {
-                        var wasPlaying = m_Player.isPlaying;
-
-                        Stop();
-
-                        if (wasPlaying)
-                            Play();
+                        m_Actor.PausePlayback();
                     }
                 }
-
-                using (new EditorGUILayout.HorizontalScope())
-                using (new EditorGUI.DisabledGroupScope(m_Clip == null || m_Controller == null))
+                else
                 {
-                    GUILayout.FlexibleSpace();
-
-                    if (m_Player.isPlaying)
+                    using (new EditorGUI.DisabledGroupScope(IsAnimationModeInExternalUse()))
                     {
-                        if (GUILayout.Button("Pause", EditorStyles.miniButton, kButtonMid))
-                            Pause();
-                    }
-                    else
-                    {
-                        using (new EditorGUI.DisabledGroupScope(IsAnimationModeInExternalUse()))
+                        if (GUILayout.Button("Play", EditorStyles.miniButton, kButtonMid))
                         {
-                            if (GUILayout.Button("Play", EditorStyles.miniButton, kButtonMid))
-                                Play();
+                            AnimationMode.StartAnimationMode();
+                            PrepareAnimationModeCurveBindings(m_Actor.controller.gameObject);
+
+                            m_Actor.StartPlayback();
                         }
                     }
-                    using (new EditorGUI.DisabledGroupScope(!(m_Player.isPlaying || m_Player.isPaused)))
+                }
+                using (new EditorGUI.DisabledGroupScope(!(m_Actor.player.isPlaying || m_Actor.player.isPaused)))
+                {
+                    if (GUILayout.Button("Stop", EditorStyles.miniButton, kButtonMid))
                     {
-                        if (GUILayout.Button("Stop", EditorStyles.miniButton, kButtonMid))
-                            Stop();
+                        m_Actor.StopPlayback();
+
+                        AnimationMode.StopAnimationMode();
                     }
                 }
             }
@@ -244,45 +419,7 @@ namespace PerformanceRecorder
 
         bool IsAnimationModeInExternalUse()
         {
-            return AnimationMode.InAnimationMode() && m_Player.isStopped;
-        }
-
-        void Play()
-        {
-            if (EditorApplication.isPlayingOrWillChangePlaymode ||
-                IsAnimationModeInExternalUse() ||
-                m_Clip == null ||
-                m_Controller == null)
-                return;
-            
-            var animator = m_Controller.GetComponent<Animator>();
-
-            if (m_Player.isStopped)
-            {
-                AnimationMode.StartAnimationMode();
-
-                var bindings = AnimationUtility.GetCurveBindings(m_Clip);
-                foreach (var binding in bindings)
-                    AnimationMode.AddEditorCurveBinding(animator.gameObject, binding);
-            }
-                
-            m_Player.Play(animator, m_Clip);
-        }
-
-        void Stop()
-        {
-            if (!m_Player.isStopped)
-            {
-                m_Player.Stop();
-
-                if (AnimationMode.InAnimationMode())
-                    AnimationMode.StopAnimationMode();
-            }
-        }
-
-        void Pause()
-        {
-            m_Player.Pause();
+            return AnimationMode.InAnimationMode() && m_Actor.state != PreviewState.None;
         }
 
         string GenerateFileName()
