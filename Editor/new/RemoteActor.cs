1bfb547babd4a02474af01c0b3e4c60bf63cbfcf
Sergi Valls <sergi@unity3d.com>
2019-08-16 19:14:14 +0800

    Refactored into separate files

---
+++ Editor/new/RemoteActor.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEditor;
+using Unity.Labs.FacialRemote;
+
+namespace PerformanceRecorder
+{
+    public enum PreviewState
+    {
+        None,
+        Playback,
+        LiveStream
+    }
+
+    [Serializable]
+    public class RemoteActor
+    {
+        static readonly string k_DefaultDirectory = "Assets/";
+        [SerializeField]
+        string m_Ip = "192.168.0.1";
+        [SerializeField]
+        int m_Port = 9000;
+        [SerializeField]
+        bool m_IsServer = false;
+        NetworkStreamSource m_NetworkStreamSource = new NetworkStreamSource();
+        AdapterSource m_AdapterSource = new AdapterSource();
+        PacketStream m_PacketStream = new PacketStream();
+        FaceDataRecorder m_Recoder = new FaceDataRecorder();
+        TakePlayer m_Player = new TakePlayer();
+        PreviewState m_PrevState = PreviewState.None;
+        [SerializeField]
+        BlendShapesController m_Controller;
+        [SerializeField]
+        AnimationClip m_Clip;
+        [SerializeField]
+        string m_Directory = k_DefaultDirectory;
+        bool m_ChangingState = false;
+
+        public string ip
+        {
+            get { return m_Ip; }
+            set { m_Ip = value; }
+        }
+
+        public int port
+        {
+            get { return m_Port; }
+            set { m_Port = value; }
+        }
+
+        public bool isServer
+        {
+            get { return m_IsServer; }
+            set { m_IsServer = value; }
+        }
+
+        public TakePlayer player
+        {
+            get { return m_Player; }
+        }
+
+        public PreviewState state
+        {
+            get { return m_PrevState; }
+        }
+
+        public BlendShapesController controller
+        {
+            get { return m_Controller; }
+            set { m_Controller = value; }
+        }
+
+        public AnimationClip clip
+        {
+            get { return m_Clip; }
+            set { m_Clip = value; }
+        }
+
+        public string directory
+        {
+            get { return m_Directory; }
+            set { m_Directory = value; }
+        }
+
+        public RemoteActor()
+        {
+            m_PacketStream.reader.faceDataChanged += FaceDataChanged;
+        }
+
+        ~RemoteActor()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            m_Recoder.StopRecording();
+            m_Player.Stop();
+            m_NetworkStreamSource.StopConnections();
+            m_PacketStream.Stop();
+            m_PacketStream.reader.faceDataChanged -= FaceDataChanged;
+        }
+
+        void SetPreviewState(PreviewState newState)
+        {
+            if (m_ChangingState)
+                return;
+            
+            if (m_PrevState == newState)
+                return;
+            
+            m_ChangingState = true;
+
+            if (m_PrevState == PreviewState.LiveStream)
+            {
+                StopRecording();
+            }
+            else if (m_PrevState == PreviewState.Playback)
+            {
+                if (m_Player.isPlaying)
+                    m_Player.Stop();
+            }
+
+            if (newState == PreviewState.LiveStream)
+            {
+
+            }
+            else if (newState == PreviewState.Playback)
+            {
+                var animator = m_Controller.GetComponent<Animator>();
+                m_Player.Play(animator, m_Clip);
+            }
+
+            m_PrevState = newState;
+            m_ChangingState = false;
+        }
+
+        public void Connect()
+        {
+            isServer = true;
+            
+            if (isServer)
+                m_NetworkStreamSource.StartServer(port);
+            else
+                m_NetworkStreamSource.ConnectToServer(ip, port);
+            
+            m_AdapterSource.streamSource = m_NetworkStreamSource;
+            m_PacketStream.streamSource = m_AdapterSource;
+            m_PacketStream.Start();
+            SetPreviewState(PreviewState.LiveStream);
+        }
+
+        public void Disconnect()
+        {
+            m_NetworkStreamSource.StopConnections();
+            m_PacketStream.Stop();
+            SetPreviewState(PreviewState.None);
+        }
+
+        public void Update()
+        {
+            m_PacketStream.reader.Receive();
+            
+            if (m_PrevState == PreviewState.Playback)
+                m_Player.Update();    
+        }
+
+        public void StartRecording()
+        {
+            if (m_PrevState != PreviewState.LiveStream)
+                return;
+
+            m_Recoder.StartRecording();
+        }
+
+        public bool IsRecording()
+        {
+            return m_Recoder.isRecording;
+        }
+
+        public void StopRecording()
+        {
+            if (!m_Recoder.isRecording)
+                return;
+            
+            m_Recoder.StopRecording();
+        }
+
+        public void WriteRecording(Stream stream)
+        {
+            using (var writer = new ARStreamWriter(stream))
+            {
+                writer.Write(m_Recoder);
+            }
+        }
+
+        public void StartLiveStream()
+        {
+            SetPreviewState(PreviewState.LiveStream);
+        }
+
+        public void StopLiveStream()
+        {
+            if (m_PrevState != PreviewState.LiveStream)
+                return;
+
+            SetPreviewState(PreviewState.None);
+        }
+
+        public void StartPlayback()
+        {
+            if (state == PreviewState.Playback)
+                m_Player.Play(controller.GetComponent<Animator>(), m_Clip);
+            else
+                SetPreviewState(PreviewState.Playback);
+        }
+
+        public void StopPlayback()
+        {
+            if (m_PrevState != PreviewState.Playback)
+                return;
+            
+            SetPreviewState(PreviewState.None);
+        }
+
+        public void PausePlayback()
+        {
+            m_Player.Pause();
+        }
+
+        void FaceDataChanged(FaceData data)
+        {
+            if (m_Controller == null)
+                return;
+
+            if (state != PreviewState.LiveStream)
+                return;
+
+            m_Controller.blendShapeInput = data.blendShapeValues;
+            m_Controller.UpdateBlendShapes();
+
+            if (m_Recoder.isRecording)
+                m_Recoder.Record(data);
+        }
+    }
+}
---
+++ Editor/new/VirtualClientWindow.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEditor;
+using Unity.Labs.FacialRemote;
+
+namespace PerformanceRecorder
+{
+    public class VirtualClientWindow : EditorWindow
+    {
+        static readonly GUILayoutOption kButtonSmall = GUILayout.Width(20f);
+        static readonly GUILayoutOption kButtonMid = GUILayout.Width(36f);
+        static readonly GUILayoutOption kButtonWide = GUILayout.Width(60f);
+        [SerializeField]
+        Vector2 m_Scroll;
+        NetworkStreamSource m_NetworkStreamSource = new NetworkStreamSource();
+        PacketStream m_PacketStream = new PacketStream();
+
+        [MenuItem("Window/Virtual Client")]
+        public static void ShowWindow()
+        {
+            var window = GetWindow(typeof(VirtualClientWindow));
+            window.titleContent = new GUIContent("Virtual Client");;
+            window.minSize = new Vector2(300, 100);
+        }
+
+        void OnGUI()
+        {
+            using (var scrollview = new EditorGUILayout.ScrollViewScope(m_Scroll))
+            {
+                m_Scroll = scrollview.scrollPosition;
+
+                using (new GUILayout.VerticalScope("box"))
+                {
+                    EditorGUILayout.LabelField("Client", EditorStyles.boldLabel);
+                    ClientGUI();
+                }
+            }
+        }
+
+        void ClientGUI()
+        {
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                GUILayout.FlexibleSpace();
+
+                if (GUILayout.Button("Connect", EditorStyles.miniButton, kButtonWide))
+                {
+                    m_NetworkStreamSource.ConnectToServer("127.0.0.1", 9000);
+                    m_PacketStream.streamSource = m_NetworkStreamSource;
+                    m_PacketStream.Start();
+                }
+
+                if (GUILayout.Button("Disconnect", EditorStyles.miniButton, kButtonWide))
+                {
+                    m_PacketStream.Stop();
+                    m_NetworkStreamSource.StopConnections();
+                }
+
+                if (GUILayout.Button("Send", EditorStyles.miniButton, kButtonMid))
+                    SendPacket();
+            }
+        }
+
+        void SendPacket()
+        {
+            /*
+            var faceData = new FaceData();
+            faceData.timeStamp = Time.realtimeSinceStartup;
+
+            for (var i = 0; i < BlendShapeValues.Count; ++i)
+                faceData.blendShapeValues[i] = UnityEngine.Random.value;
+            
+            m_PacketStream.writer.Write(faceData);
+            */
+
+            var data = new StreamBufferDataV1();
+            data.FrameTime = Time.realtimeSinceStartup;
+
+            for (var i = 0; i < BlendShapeValues.Count; ++i)
+                data.BlendshapeValues[i] = UnityEngine.Random.value;
+
+            m_PacketStream.writer.Write(data.ToBytes(), Marshal.SizeOf<StreamBufferDataV1>());
+        }
+    }
+}
