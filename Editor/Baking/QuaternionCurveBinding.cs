9b2fc1ddd5499e5942edeaf1be44a85e061459e0
Sergi Valls <sergi@unity3d.com>
2019-08-21 17:52:36 +0800

    Fix QuaternionCurveBinding

--- Editor/Baking/QuaternionCurveBinding.cs
+++ Editor/Baking/QuaternionCurveBinding.cs
@@ -6,6 +6,7 @@ namespace Unity.Labs.FacialRemote
 {
     public class QuaternionCurveBinding
     {
+        const int kCurveCount = 4;
         string[] m_PropertyNames;
         AnimationCurve[] m_Curves;
         string m_Path;
@@ -15,22 +16,22 @@ namespace Unity.Labs.FacialRemote
         {
             m_Path = path;
             m_Type = type;
-            m_PropertyNames = new string[4]
+            m_PropertyNames = new string[kCurveCount]
             {
                 propertyName + ".x",
                 propertyName + ".y",
                 propertyName + ".z",
                 propertyName + ".w"
             };
-            m_Curves = new AnimationCurve[4];
+            m_Curves = new AnimationCurve[kCurveCount];
 
-            for (var i = 0; i < 4; ++i)
+            for (var i = 0; i < kCurveCount; ++i)
                 m_Curves[i] = new AnimationCurve();
         }
 
         public void AddKey(float time, Quaternion value)
         {
-            for (var i = 0; i < 4; ++i)
+            for (var i = 0; i < kCurveCount; ++i)
             {
                 var curve = m_Curves[i];
                 curve.AddKey(time, value[i]);
@@ -39,7 +40,7 @@ namespace Unity.Labs.FacialRemote
 
         public void SetCurves(AnimationClip clip)
         {
-            for (var i = 0; i < 3; ++i)
+            for (var i = 0; i < kCurveCount; ++i)
                 clip.SetCurve(m_Path, m_Type, m_PropertyNames[i], m_Curves[i]);
         }
     }
